96468d8fab464f5484a0bd4065a5a3aba20c4e28
Dylan <dquesada186@gmail.com>
2017-11-29 11:50:17 -0600

    added function that returns array of compressed string

--- stringCompressor.js
+++ stringCompressor.js
@@ -1,9 +1,30 @@
-function userInput(message){
-	if(message != null){
-		return prompt(message);
+// function userInput(message){
+// 	if(message != null){
+// 		return prompt(message);
+// 	}
+// 	else{
+// 		return prompt("Enter user input:");
+// 	}
+// }
+
+function stringCompressor(string){
+	var output = [];
+	let counter = 1;
+	let arrayCounter = 0;
+	for (var i = 0; i < string.length; i++){
+		
+		if(string.charAt(i) === string.charAt(i + 1)){
+			counter ++;
+		}
+		else{
+			output[arrayCounter] = counter + string.charAt(i);
+			counter = 1;
+			arrayCounter++;
+		}
 	}
-	else{
-		return prompt("Enter user input:");
-	}
+	return output;
 }
 
+
+var x = "aaabbccccd";
+console.log(stringCompressor(x));
